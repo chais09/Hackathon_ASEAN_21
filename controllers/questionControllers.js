95375e31b276e271ed0966cddb8834ec35c9263e
chaisengl <chaisengl@student.unimelb.edu.au>
2021-09-04 23:19:22 +1000

    added video1.hbs

--- controllers/questionControllers.js
+++ controllers/questionControllers.js
@@ -1,4 +1,5 @@
 const {questions} = require('../models/questionModels.js')
+const {cusAnswers} = require('../models/questionModels.js')
 
 const getQuesNum = async (req, res) => {
     let question = await questions.findOne({questionNo: req.params.id},{questionNo:true, questionQues: true, answer: true});
@@ -13,11 +14,44 @@ const getQuesNum = async (req, res) => {
     // "d. $9,000");
     // newUser.answer = " ";
     // // and save the user
-    // newUser.save()
-      
+    // newUser.save()     
 }
+
+const markAnswer = async (req, res) => {
+
+  cus_ans = req.body.cus_ans;
+  cus_id = req.session.passport.user._id
+  ques_id = req.params.id;
+  ques = questions.findOne({_id: ques_id},{questionNo: true, questionQues: true, answer: true})
+  ques_ques = ques.questionQues
+  ques_ans = ques.answer
+  ques_no = ques.questionNo
+
+  newCusAnswer = new cusAnswers();
+  newCusAnswer.quesID = ques_id;
+  newCusAnswer.cusID = cus_id;
+  newCusAnswer.cusAns = cus_ans;
+  newCusAnswer.ques_no = ques_no
+  newCusAnswer.ques_ques = ques_ques
+  newCusAnswer.ques_ans = ques_ans;
+
+  newCusAnswer.save();
+
+  let tf;
+  if (ques.ans!=cus_ans){
+    tf = "false"
+  }
+  else{
+    tf = "true"
+  }
+
+  res.render("asdd", {trueFalse: tf, ques_ques : ques_ques, ques_no : ques_no,
+                       ques_ans: ques_ans, cus_ans: cus_ans})
+}
+
 
 
   module.exports = {
     getQuesNum,
+    markAnswer,
   };
